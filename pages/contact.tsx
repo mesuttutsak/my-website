e18510b8dbc2e8d0df2c8b373c4d40a55812fd43
Mesut Tutsak <ttsk.mesut@gmail.com>
2023-08-07 13:08:01 +0300

    contact sayfası oluşturuldu

---
+++ pages/contact.tsx
@@ -0,0 +1,29 @@
+import HomePageComponent from '@/src/components/HomePage';
+import MainLayout from '@/src/layout/MainLayout';
+import Head from 'next/head';
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <>
+      <Head>
+        <title>Mesut Tutsak - Frontend Developer</title>
+        <meta name="description" content="Benim adım Mesut Tutsak, bir Frontend Developer'ım ve bu benim kişisel websitesidir." />
+        <meta name="keywords" content="Mesut Tutsak, Frontend Developer, web developer, websitesi, frontend, HTML, CSS, JavaScript" />
+        <meta name="author" content="Mesut Tutsak" />
+
+        {/* <!-- Favicon --> */}
+        <link rel="icon" href="https://www.mesuttutsak.dev/favicon.ico" sizes="any" />
+        <link rel="apple-touch-icon" sizes="180x180" href="https://www.mesuttutsak.dev/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="https://www.mesuttutsak.dev/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="https://www.mesuttutsak.dev/favicon-16x16.png"/>
+        <link rel="manifest" href="https://www.mesuttutsak.dev/site.webmanifest"/>
+      </Head>
+      <MainLayout>
+        deneme
+      </MainLayout>
+    </>
+  )
+}
+
+export default Contact
