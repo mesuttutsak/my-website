a71322f585375e4abfecaebbc283655a9f9767e5
Mesut Tutsak <ttsk.mesut@gmail.com>
2026-03-20 19:42:39 +0300

    feat: enhance pointer position handling and light beam visibility; optimize performance with refs and media queries

--- src/ui/LightBeam/LightBeam.tsx
+++ src/ui/LightBeam/LightBeam.tsx
@@ -1,19 +1,11 @@
 'use client'
+
 import { usePointerPosition } from "@/src/shared/hooks/usePointerPosition";
-import { cn } from "@/src/shared/lib/cn";
 
-const LightBeam: React.FC = () => {
-  const { x, y, hasMoved } = usePointerPosition();
+const LightBeam = () => {
+  const beamRef = usePointerPosition();
 
-  return (
-    <div
-      className={cn('lightBeam', hasMoved && 'show')}
-
-      style={{
-        background: `radial-gradient(100vw at ${x}px ${y}px, rgba(241, 245, 249, 1), transparent 50%)`,
-      }}
-    ></div>
-  );
+  return <div aria-hidden="true" className="lightBeam" ref={beamRef}></div>;
 };
 
 export default LightBeam;
